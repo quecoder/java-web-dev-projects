b7e31862a9b22c8e016787629e0ed246a1ae11e1
quecoder <qmkornegay@gmail.com>
2023-12-07 19:33:53 -0600

    Character Counting Studio

--- control-flow-and-collections/exercises/collection-exercises/src/main/java/org/launchcode/ArrayListPractice.java
+++ control-flow-and-collections/exercises/collection-exercises/src/main/java/org/launchcode/ArrayListPractice.java
@@ -44,39 +44,39 @@ public class ArrayListPractice {
         ArrayList<String> words = new ArrayList<>();
         for (int number : arr) {
             words.add(Integer.toString(number));
-//            words.add("To"); // 2
-//            words.add("Me"); // 2
-//            words.add("Hi"); // 2
-//            words.add("Ape"); // 3
-//            words.add("Ten"); // 3
-//            words.add("Fly"); // 3
-//            words.add("Four"); // 4
-//            words.add("Play"); // 4
-//            words.add("Flag"); // 4
-//            words.add("Apple"); // 5
-//            words.add("Maple"); // 5
-//            words.add("Later"); // 5
-//            words.add("Banana"); // 6
-//            words.add("Blower"); // 6
-//            words.add("Titans"); // 6
-//            words.add("Alabama"); // 7
-//            words.add("Country"); // 7
-//            words.add("Inspire"); // 7
-//            words.add("Cellular"); // 8
-//            words.add("Bookworm"); // 8
-//            words.add("Swimming"); // 8
-//            words.add("Crocodile"); // 9
-//            words.add("Commander"); // 9
-//            words.add("Framework"); // 9
-//            words.add("Validation"); // 10
-//            words.add("Washington"); // 10
-//            words.add("Toothbrush"); // 10
+            words.add("To"); // 2
+            words.add("Me"); // 2
+            words.add("Hi"); // 2
+            words.add("Ape"); // 3
+            words.add("Ten"); // 3
+            words.add("Fly"); // 3
+            words.add("Four"); // 4
+            words.add("Play"); // 4
+            words.add("Flag"); // 4
+            words.add("Apple"); // 5
+            words.add("Maple"); // 5
+            words.add("Later"); // 5
+            words.add("Banana"); // 6
+            words.add("Blower"); // 6
+            words.add("Titans"); // 6
+            words.add("Alabama"); // 7
+            words.add("Country"); // 7
+            words.add("Inspire"); // 7
+            words.add("Cellular"); // 8
+            words.add("Bookworm"); // 8
+            words.add("Swimming"); // 8
+            words.add("Crocodile"); // 9
+            words.add("Commander"); // 9
+            words.add("Framework"); // 9
+            words.add("Validation"); // 10
+            words.add("Washington"); // 10
+            words.add("Toothbrush"); // 10
         }
 
         System.out.println("Words with " + length + " letters:");
         for (String word : words) {
             if (word.length() == length) {
-                System.out.println(words);
+                System.out.println("Words with the number " + length + " are:" + words);
             }
         }
     }
