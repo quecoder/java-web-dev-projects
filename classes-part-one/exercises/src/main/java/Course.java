9376e91eab606ee787f92b88e57a7567c11b217c
quecoder <qmkornegay@gmail.com>
2024-01-02 19:51:25 -0600

    Classes pt 1 Excercise

---
+++ classes-part-one/exercises/src/main/java/Course.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class Course {
+    private String courseName;
+    private int courseId;
+    private ArrayList<Student> enrolledStudents;
+
+    public String getCourseName() {
+        return courseName;
+    }
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(int courseId) {
+        this.courseId = courseId;
+    }
+
+    public ArrayList<Student> getEnrolledStudents() {
+        return enrolledStudents;
+    }
+
+    public void setEnrolledStudents(ArrayList<Student> enrolledStudents) {
+        this.enrolledStudents = enrolledStudents;
+    }
+}
--- classes-part-one/exercises/src/main/java/StudentPractice.java
+++ classes-part-one/exercises/src/main/java/StudentPractice.java
@@ -1,5 +1,9 @@
 public class StudentPractice {
     public static void main(String[] args){
         //insantiate your Student class below
+        Student quintonKornegay = new Student();
+        quintonKornegay.setName("Quinton Kornegay");
+        quintonKornegay.setNumberOfCredits(1);
+        quintonKornegay.setGpa(4.0);
     }
 }
