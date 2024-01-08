cbd570251731eb803b8716a760547230366ee2ad
quecoder <qmkornegay@gmail.com>
2024-01-08 15:20:23 -0600

    inheritanceStudio - Complete

---
+++ inheritance/Studio/src/main/java/questions/Checkbox.java
@@ -0,0 +1,27 @@
+package questions;
+
+public class Checkbox extends Question {
+    public Checkbox (String question, Choice[] choiceArray) {
+        super(question, choiceArray);
+        setMaxResponses(getNumCorrect());
+    }
+
+    @Override
+    public String toString() {
+        String newline = System.lineSeparator();
+        return newline + "CHOOSE ONE OR MORE:" + newline +
+                getQuestion() + newline +
+                getFormattedChoices();
+    }
+
+    public int getNumCorrect() {
+        int numCorrect = 0;
+        for (Choice choice : getChoiceMap().values()) {
+            if (choice.isCorrect()) {
+                numCorrect++;
+            }
+        }
+        return numCorrect;
+    }
+
+}
